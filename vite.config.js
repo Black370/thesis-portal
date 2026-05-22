f3c9d5d607dbe01bbf147778890acbc7899e5cae
Black370 <johnjame052399@gmail.com>
2026-05-22 14:01:13 +0700

    Prepare frontend for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // This tells Vite to prepend '/thesis-portal/' to all your asset links
-  base: '/thesis-portal/',
+  plugins: [react()]
+  // Deleted: base: '/thesis-portal/'
 })
